eca849f8bb9837f6ef2f0b910129fe1ccdf18a9a
Nazar Tytskyi <Nazar_Tytskyi@epam.com>
2021-12-17 16:47:42 +0200

    'upd'

--- index.js
+++ index.js
@@ -3,4 +3,17 @@ const chat = document.getElementById('chat');
 const input = document.getElementById('input');
 const sendBtn = document.getElementById('send');
 
-const ws = new WebSocket('ws//localhost:8000')
+const ws = new WebSocket('ws//localhost:8000');
+
+
+function addMessage(author, date, text, isMyMessage) {
+    const msg = `
+     <div class="message ${isMyMessage ? 'message-right ' : ''} d-flex flex-column">
+        <div class="message-author">${author}</div>
+        <div class="message-text">${text}</div>
+        <div class="date">${date}</div>
+    </div>
+    `;
+
+    chat.innerHTML = chat.innerHTML + msg;
+}
