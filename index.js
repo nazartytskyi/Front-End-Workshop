c7b6f220f02fca96ccd44a1f7ff12b553a46b0db
Nazar Tytskyi <Nazar_Tytskyi@epam.com>
2021-12-17 16:41:22 +0200

    'upd'

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const name = prompt('What is your name?');
 const chat = document.getElementById('chat');
 const input = document.getElementById('input');
+const sendBtn = document.getElementById('send');
+
+const ws = new WebSocket('ws//localhost:8000')
